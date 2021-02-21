97cacd9106bc4c686e4d004041dbc8c723578adc
Arvydas Baranauskas <arvydas.baranauskas@hashtages.com>
2021-02-22 01:02:27 +0200

    allow jsx transformations for preset react fot better handing of semantic ui for the future :)

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+
 module.exports = {
-    entry: './src/index.jsx',
-    output: {
-        filename: 'app.js',
-        path: __dirname + 'build',
-        publicPath: '/'
-    },
-    devServer: {
-        contentBase: './build'
-    },
-    devtool: 'eval-source-map',
+    entry: path.resolve(__dirname, './src/index.js'),
     mode: 'development',
     module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
-            }
-        ]
-    }
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx'],
+    },
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'bundle.js',
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()],
+    devServer: {
+      contentBase: path.resolve(__dirname, './dist'),
+      hot: true,
+    },
 }
